53e71d2d857f3fa7b7ef776fae032da79d97f3fe
Adeel Shabbir <54504931+madeelshabbir@users.noreply.github.com>
2023-10-02 09:47:06 +0500

    B-5: Update inventory quantity endpoint (#5)
    * alembic/versions/c8d7b5cd6fdd_rename_inventories_stock_quantity_to_quantity.py: Removed comment.
    * app/models/inventory.py: Added event to auto update updated_at field.
    * app/routers/v1/inventories.py: Created endpoint to update and get inventory.

--- app/routers/v1/inventories.py
+++ app/routers/v1/inventories.py
@@ -1,6 +1,7 @@
 from app.models import Product, Inventory
 from app.utils.database import SessionLocal
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, HTTPException, Path, Query
+from pydantic import BaseModel
 from typing import Optional
 
 inventories_router = APIRouter()
@@ -36,3 +37,51 @@ async def get_inventories(
   ]
 
   return response_data
+
+@inventories_router.get('/{id}')
+async def get_inventory(
+  id: int = Path(..., description='Inventory ID for retrieving the inventory')
+):
+  db = SessionLocal()
+  inventory = db.query(Inventory).filter(Inventory.id == id).first()
+
+  if inventory is None:
+    raise HTTPException(status_code=404, detail=f"Inventory with ID {id} not found")
+
+  product = inventory.product
+
+  return {
+    'id': inventory.id,
+    'quantity': inventory.quantity,
+    'updated_at': inventory.updated_at,
+    'product': {
+      'id': product.id,
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'created_at': product.created_at,
+      'category_id': product.category_id,
+    }
+  }
+
+class UpdateInventoryRequestModel(BaseModel):
+  quantity: int
+
+@inventories_router.patch('/{id}')
+async def update_inventory(
+  id: int = Path(..., description='Inventory ID of the inventory resource'),
+  body: UpdateInventoryRequestModel = ...
+):
+  db = SessionLocal()
+  inventory = db.query(Inventory).filter(Inventory.id == id).first()
+
+  if inventory is None:
+    db.close()
+    raise HTTPException(status_code=404, detail=f"Inventory with ID {id} not found")
+
+  inventory.quantity = body.quantity
+  db.commit()
+  db.refresh(inventory)
+  db.close()
+
+  return inventory
